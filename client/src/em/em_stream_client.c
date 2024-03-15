PlutoVR/electric-maple

Client receive message from header extension

client/src/em/em_stream_client.c
@@ -14,6 +14,9 @@
 #include "gst_common.h" // for em_sample
 #include "em/em_egl.h"
 
+#include <openxr/openxr.h>
+#include <pluto.pb.h>
+
 #include "os/os_threading.h"
 
 #include <gst/app/gstappsink.h>
@@ -26,7 +29,9 @@
 #include <gst/gstmessage.h>
 #include <gst/gstsample.h>
 #include <gst/gstutils.h>
+#include <gst/rtp/gstrtpbuffer.h>
 #include <gst/video/video-frame.h>
+#include <pb_decode.h>
 
 #include <EGL/egl.h>
 #include <GLES2/gl2ext.h>
@@ -586,6 +591,67 @@ em_stream_client_stop(EmStreamClient *sc)
 	sc->pipeline_is_running = false;
 }
 
+static inline bool
+em_stream_client_extract_frame_data(GstBuffer *buffer, pluto_DownMessage *msg)
+{
+
+	GstRTPBuffer rtp_buffer = GST_RTP_BUFFER_INIT;
+
+	// extract Downstream metadata from rtp header
+	if (!gst_rtp_buffer_map(buffer, GST_MAP_WRITE, &rtp_buffer)) {
+		ALOGE("Failed to map GstBuffer");
+		return false;
+	}
+
+	// Not all buffers has extension data attached, check.
+	if (!gst_rtp_buffer_get_extension(&rtp_buffer)) {
+		goto no_buf;
+	}
+	uint8_t buf[pluto_DownMessage_size] = {};
+	guint size = 0;
+	if (!gst_rtp_buffer_get_extension_twobytes_header(&rtp_buffer, NULL, RTP_TWOBYTES_HDR_EXT_ID,
+	                                                  0 /* NOTE: We do not support multi-extension-elements.*/,
+	                                                  &buf, &size)) {
+
+		ALOGE("Could not retrieve twobyte rtp extension on buffer!");
+		goto no_buf;
+	}
+	pb_istream_t our_istream = pb_istream_from_buffer(buf, size);
+
+	bool result = pb_decode_ex(&our_istream, pluto_DownMessage_fields, msg, PB_DECODE_NULLTERMINATED);
+
+	if (!result) {
+		ALOGE("Error! %s", PB_GET_ERROR(&our_istream));
+		goto no_buf;
+	}
+
+	gst_rtp_buffer_unmap(&rtp_buffer);
+	return true;
+
+no_buf:
+	gst_rtp_buffer_unmap(&rtp_buffer);
+	return false;
+}
+
+static inline XrQuaternionf
+quat_to_openxr(const pluto_Quaternion *q)
+{
+	return (XrQuaternionf){q->x, q->y, q->z, q->w};
+}
+static inline XrVector3f
+vec3_to_openxr(const pluto_Vec3 *v)
+{
+	return (XrVector3f){v->x, v->y, v->z};
+}
+
+static inline XrPosef
+pose_to_openxr(const pluto_Pose *p)
+{
+	return (XrPosef){
+	    p->has_orientation ? quat_to_openxr(&p->orientation) : (XrQuaternionf){0, 0, 0, 1},
+	    p->has_position ? vec3_to_openxr(&p->position) : (XrVector3f){0, 0, 0},
+	};
+}
 struct em_sample *
 em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode_end)
 {
@@ -614,6 +680,8 @@ em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode
 	}
 	*out_decode_end = decode_end;
 
+	struct em_sc_sample *ret = calloc(1, sizeof(struct em_sc_sample));
+
 	// ALOGE("FRED: GOT A SAMPLE !!!");
 	GstBuffer *buffer = gst_sample_get_buffer(sample);
 	GstCaps *caps = gst_sample_get_caps(sample);
@@ -621,28 +689,22 @@ em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode
 
 	// extract Downstream metadata from rtp header
 	pluto_DownMessage msg = pluto_DownMessage_init_default;
+	if (em_stream_client_extract_frame_data(buffer, &msg)) {
+		ALOGI("RYLIE: got downstream frame message");
+		if (msg.has_frame_data && msg.frame_data.has_P_localSpace_view0 &&
+		    msg.frame_data.has_P_localSpace_view1) {
+			// OK we have a message for this one.
+			ALOGI("RYLIE: got downstream frame message with poses!");
+			// TODO is it too late to get it here?
+			ret->base.have_poses = true;
+			ret->base.poses[0] = pose_to_openxr(&msg.frame_data.P_localSpace_view0);
+			ret->base.poses[1] = pose_to_openxr(&msg.frame_data.P_localSpace_view1);
 
-	if (!gst_rtp_buffer_map(buffer, GST_MAP_WRITE, &rtp_buffer)) {
-		ALOGE("Failed to map GstBuffer");
-	}
-
-	// Not all buffers has extension data attached, check.
-	if (gst_rtp_buffer_get_extension(&rtp_buffer)) {
-		if (!gst_rtp_buffer_get_extension_twobytes_header (&rtp_buffer,
-				NULL,
-				RTP_TWOBYTES_HDR_EXT_ID,
-				0 /* NOTE: We do not support multi-extension-elements.*/,
-				&msg,
-				sizeof(msg))) {
-			ALOGE("Could not retrieve twobyte rtp extension on buffer!");
+			// TODO: use msg.frame_id (and others) and populate properly inside stream client.
+			// ...
+			// ...
 		}
 	}
-
-	gst_rtp_buffer_unmap(&rtp_buffer);
-
-	// TODO: use msg.frame_id (and others) and populate properly inside stream client.
-	// ...
-	// ...
 
 	GstVideoInfo info;
 	gst_video_info_from_caps(&info, caps);
@@ -657,8 +719,6 @@ em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode
 		sc->height = height;
 	}
 #endif
-
-	struct em_sc_sample *ret = calloc(1, sizeof(struct em_sc_sample));
 
 	GstVideoFrame frame;
 	GstMapFlags flags = (GstMapFlags)(GST_MAP_READ | GST_MAP_GL);

PlutoVR/linux-streaming

client/src/em/em_stream_client.c
@@ -14,6 +14,10 @@
 #include "gst_common.h" // for em_sample
 #include "em/em_egl.h"
 
+#include "electricmaple.pb.h"
+
+#include <openxr/openxr.h>
+
 #include "os/os_threading.h"
 
 #include <gst/app/gstappsink.h>
@@ -26,7 +30,9 @@
 #include <gst/gstmessage.h>
 #include <gst/gstsample.h>
 #include <gst/gstutils.h>
+#include <gst/rtp/gstrtpbuffer.h>
 #include <gst/video/video-frame.h>
+#include <pb_decode.h>
 
 #include <EGL/egl.h>
 #include <GLES2/gl2ext.h>
@@ -263,6 +269,68 @@ em_stream_client_class_init(EmStreamClientClass *klass)
 
 #endif
 
+static inline bool
+em_stream_client_extract_frame_data(GstBuffer *buffer, em_proto_DownMessage *msg)
+{
+
+	GstRTPBuffer rtp_buffer = GST_RTP_BUFFER_INIT;
+
+	// extract Downstream metadata from rtp header
+	if (!gst_rtp_buffer_map(buffer, GST_MAP_WRITE, &rtp_buffer)) {
+		ALOGE("Failed to map GstBuffer");
+		return false;
+	}
+
+	// Not all buffers has extension data attached, check.
+	if (!gst_rtp_buffer_get_extension(&rtp_buffer)) {
+		goto no_buf;
+	}
+	uint8_t buf[em_proto_DownMessage_size] = {};
+	guint size = 0;
+	if (!gst_rtp_buffer_get_extension_twobytes_header(&rtp_buffer, NULL, RTP_TWOBYTES_HDR_EXT_ID,
+	                                                  0 /* NOTE: We do not support multi-extension-elements.*/,
+	                                                  &buf, &size)) {
+
+		ALOGE("Could not retrieve twobyte rtp extension on buffer!");
+		goto no_buf;
+	}
+	pb_istream_t our_istream = pb_istream_from_buffer(buf, size);
+
+	bool result = pb_decode_ex(&our_istream, em_proto_DownMessage_fields, msg, PB_DECODE_NULLTERMINATED);
+
+	if (!result) {
+		ALOGE("Error! %s", PB_GET_ERROR(&our_istream));
+		goto no_buf;
+	}
+
+	gst_rtp_buffer_unmap(&rtp_buffer);
+	return true;
+
+no_buf:
+	gst_rtp_buffer_unmap(&rtp_buffer);
+	return false;
+}
+
+static inline XrQuaternionf
+quat_to_openxr(const em_proto_Quaternion *q)
+{
+	return (XrQuaternionf){q->x, q->y, q->z, q->w};
+}
+static inline XrVector3f
+vec3_to_openxr(const em_proto_Vec3 *v)
+{
+	return (XrVector3f){v->x, v->y, v->z};
+}
+
+static inline XrPosef
+pose_to_openxr(const em_proto_Pose *p)
+{
+	return (XrPosef){
+	    p->has_orientation ? quat_to_openxr(&p->orientation) : (XrQuaternionf){0, 0, 0, 1},
+	    p->has_position ? vec3_to_openxr(&p->position) : (XrVector3f){0, 0, 0},
+	};
+}
+
 /*
  * callbacks
  */
@@ -348,6 +416,7 @@ on_new_sample_cb(GstAppSink *appsink, gpointer user_data)
 	GstSample *prevSample = NULL;
 	GstSample *sample = gst_app_sink_pull_sample(appsink);
 	g_assert_nonnull(sample);
+
 	{
 		g_autoptr(GMutexLocker) locker = g_mutex_locker_new(&sc->sample_mutex);
 		prevSample = sc->sample;
@@ -614,32 +683,30 @@ em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode
 	}
 	*out_decode_end = decode_end;
 
+	struct em_sc_sample *ret = calloc(1, sizeof(struct em_sc_sample));
+
 	// ALOGE("FRED: GOT A SAMPLE !!!");
 	GstBuffer *buffer = gst_sample_get_buffer(sample);
 	GstCaps *caps = gst_sample_get_caps(sample);
 	GstRTPBuffer rtp_buffer = GST_RTP_BUFFER_INIT;
 
 	// extract Downstream metadata from rtp header
-	pluto_DownMessage msg = pluto_DownMessage_init_default;
-
-	if (!gst_rtp_buffer_map(buffer, GST_MAP_WRITE, &rtp_buffer)) {
-		ALOGE("Failed to map GstBuffer");
+	em_proto_DownMessage msg = em_proto_DownMessage_init_default;
+	if (em_stream_client_extract_frame_data(buffer, &msg)) {
+		ALOGI("RYLIE: got downstream frame message");
 	}
+	if (msg.has_frame_data && msg.frame_data.has_P_localSpace_view0 && msg.frame_data.has_P_localSpace_view1) {
+		// OK we have a message for this one.
+		ALOGI("RYLIE: got downstream frame message with poses!");
+		// TODO is it too late to get it here?
+		ret->base.have_poses = true;
+		ret->base.poses[0] = pose_to_openxr(&msg.frame_data.P_localSpace_view0);
+		ret->base.poses[1] = pose_to_openxr(&msg.frame_data.P_localSpace_view1);
 
-	// Not all buffers has extension data attached, check.
-	if (gst_rtp_buffer_get_extension(&rtp_buffer)) {
-		if (!gst_rtp_buffer_get_extension_twobytes_header(
-		        &rtp_buffer, NULL, RTP_TWOBYTES_HDR_EXT_ID,
-		        0 /* NOTE: We do not support multi-extension-elements.*/, &msg, sizeof(msg))) {
-			ALOGE("Could not retrieve twobyte rtp extension on buffer!");
-		}
+		// TODO: use msg.frame_id (and others) and populate properly inside stream client.
+		// ...
+		// ...
 	}
-
-	gst_rtp_buffer_unmap(&rtp_buffer);
-
-	// TODO: use msg.frame_id (and others) and populate properly inside stream client.
-	// ...
-	// ...
 
 	GstVideoInfo info;
 	gst_video_info_from_caps(&info, caps);
@@ -654,8 +721,6 @@ em_stream_client_try_pull_sample(EmStreamClient *sc, struct timespec *out_decode
 		sc->height = height;
 	}
 #endif
-
-	struct em_sc_sample *ret = calloc(1, sizeof(struct em_sc_sample));
 
 	GstVideoFrame frame;
 	GstMapFlags flags = (GstMapFlags)(GST_MAP_READ | GST_MAP_GL);